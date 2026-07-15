4e699d65c0f9932542226d99fc7d90a281ffeb8a
Rampje-074 <ramonkissing17@gmail.com>
2026-07-15 20:01:01 +0200

    Cache verversen naar versie 1.5

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'burgemeester-spel-v1-4';
+const CACHE_NAME = 'burgemeester-spel-v1-5';
 const CORE_FILES = ['./', './index.html', './manifest.webmanifest', './app-icon.svg', './clipboard.png', './clipboard.jpeg'];
 
 self.addEventListener('install', event => {
